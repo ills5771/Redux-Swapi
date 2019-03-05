0ef60060a14ee5444d40b64e704909f7d65012a1
ills5771 <ilyayelizarov1@gmail.com>
2019-03-05 13:55:40 -0800

    changed res.data  to res.data.results in action creator  to get the chars to display

--- src/actions/index.js
+++ src/actions/index.js
@@ -9,9 +9,11 @@ export const fetchingCharacters = () => dispatch => {
   dispatch({ type: FETCHING });
   axios
     .get("https://swapi.co/api/people")
-    .then(res => dispatch({ type: FETCHING_SUCCESS, payload: res.data }))
+    .then(res =>
+      dispatch({ type: FETCHING_SUCCESS, payload: res.data.results })
+    )
     .catch(err => dispatch({ type: FETCHING_FAILURE, payload: err }));
 };
 // our action creator will be a function that returns a function
 // the url to fetch characters from is `https://swapi.co/api/people/`
-// remember that now we have controll over our thunk-based action creator
+// remember that now we have control over our thunk-based action creator
--- src/views/CharacterListView.js
+++ src/views/CharacterListView.js
@@ -2,7 +2,7 @@ import React from "react";
 import { connect } from "react-redux";
 
 import { CharacterList } from "../components";
-// import actions
+import { fetchingCharacters } from "../actions";
 
 class CharacterListView extends React.Component {
   constructor() {
@@ -10,12 +10,12 @@ class CharacterListView extends React.Component {
   }
 
   componentDidMount() {
-    // call our action
+    this.props.fetchingCharacters();
   }
 
   render() {
     if (this.props.fetching) {
-      // return something here to indicate that you are fetching data
+      ("fetching characters...");
     }
     return (
       <div className="CharactersList_wrapper">
@@ -25,11 +25,14 @@ class CharacterListView extends React.Component {
   }
 }
 
-// our mapStateToProps needs to have two properties inherited from state
-// the characters and the fetching boolean
+const mapStateToProps = state => ({
+  characters: state.charsReducer.characters,
+  isFetching: state.charsReducer.isFetching
+});
+
 export default connect(
-  null /* mapStateToProps replaces null here */,
+  mapStateToProps,
   {
-    /* action creators go here */
+    fetchingCharacters
   }
 )(CharacterListView);
