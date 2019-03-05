ec65473c5d6b80af0efddfe7e5939ac98da3c8c2
ills5771 <ilyayelizarov1@gmail.com>
2019-03-05 13:17:55 -0800

    added cases for FETCHING,FETCHING_SUCCESS,FETCHING_FAILURE

--- src/reducers/starWarsReducer.js
+++ src/reducers/starWarsReducer.js
@@ -1,10 +1,31 @@
-import /* we need our action types here*/ "../actions";
+import { FETCHING, FETCHING_SUCCESS, FETCHING_FAILURE } from "../actions";
 const initialState = {
-  characters: []
+  characters: [],
+  isFetching: false,
+  error: null
   // Array characters, Boolean fetching, null error.
 };
 export const charsReducer = (state = initialState, action) => {
   switch (action.type) {
+    case FETCHING:
+      return {
+        ...state,
+        error: "",
+        isFetching: true
+      };
+    case FETCHING_SUCCESS:
+      return {
+        ...state,
+        characters: action.payload,
+        isFetching: false,
+        error: ""
+      };
+    case FETCHING_FAILURE:
+      return {
+        ...state,
+        error: action.payload,
+        isFetching: false
+      };
     // Fill me in with the important reducers
     // action types should be FETCHING, SUCCESS and FAILURE
     // your switch statement should handle all of these cases.
